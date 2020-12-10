f84331e0d7369fa5da3cbbbb64db70a85bbeb6a0
chas-e <sigmapi76388@gmail.com>
2020-12-10 12:54:45 -0500

    adds logic for comments and likes

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,29 @@
+const { UserInputError } = require('apollo-server');
+
+const Post = require('../../models/Post');
+
+module.exports = {
+    Mutation: {
+        createComment: async (_, { postId, body }, context) => {
+            const user = checkAuth(context);
+            if(body.trim() === '') {
+                throw new UserInputError('Empty comment', {
+                    errors: {
+                        body: 'Comment must not be empty'
+                    }
+                });
+            }
+            const post = await Post.findById(postId);
+
+            if(post) {
+                post.comments.unshift({
+                    body,
+                    username,
+                    createdAt: new Date().toISOString()
+                });
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found');
+        }
+    }
+}
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,11 +1,24 @@
 const { gql } = require('apollo-server');
 
 module.exports = gql`
-type Post{
+type Post {
     id: ID!
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
+}
+type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+}
+type Like {
+   id: ID!
+   createdAt: String!
+   username: String! 
 }
 type User {
     id: ID!
@@ -29,5 +42,8 @@ input RegisterInput {
         login(username: String!, password: String!): User!
         createPost(body: String!): Post!
         deletePost(postId: ID!): String!
+        createComment(postId: ID!, body: String!): Post!
+        deleteComment(postId: ID!, commentId: ID!): Post!
+        likePost(opstId: ID!): Post!
     }
 `
