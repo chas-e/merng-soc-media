cfd1823c7ddec058f6f7c4179bb83ba266be0b0d
chas-e <sigmapi76388@gmail.com>
2020-12-19 16:51:44 -0500

    adds navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
+import Home from './pages/Home.jsx';
+import Register from './pages/Register.jsx';
+import Login from './pages/Login.jsx';
+import MenuBar from './components/MenuBar.jsx';
+
 function App() {
   return (
     <Router>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/register' component={Register} />
-
+      <Container>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Container>
     </Router>
   );
 }
